614a60c26a6b3080a070cbd4bd2b48c912ac552e
Daniel Triendl <daniel@pew.cc>
2018-06-13 19:28:31 +0200

    Updated Java-WebSocket to 1.3.8

--- socket.io-java-client/src/main/java/io/socket/WebsocketTransport.java
+++ socket.io-java-client/src/main/java/io/socket/WebsocketTransport.java
@@ -6,9 +6,7 @@ import java.net.URL;
 import java.util.regex.Pattern;
 
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSocketFactory;
 
-import org.java_websocket.client.DefaultSSLWebSocketClientFactory;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 
@@ -30,7 +28,11 @@ class WebsocketTransport extends WebSocketClient implements IOTransport {
         this.connection = connection;
         SSLContext context = IOConnection.getSslContext();
         if("wss".equals(uri.getScheme()) && context != null) {
-	        this.setWebSocketFactory(new DefaultSSLWebSocketClientFactory(context));
+            try {
+                this.setSocket(context.getSocketFactory().createSocket());
+            } catch (IOException e) {
+                throw new RuntimeException("Error creating secure socket", e);
+            }
         }
     }
 
